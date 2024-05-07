a99d708398921c8c596ad7bb7d805839323f4b31
gokilp <gokilp25@gmail.com>
2024-05-07 08:51:07 +0530

    fix api error

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let btc = document.querySelector("#Bitcoin");
+let eth = document.querySelector("#Ethereum");
+let doge = document.querySelector("#dogecoin");
+
 var settings = {
-    "async": true,
-    "crossDomain": true, // Corrected typo in "crossDomain"
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-    "method": "GET",
-    "headers": {}
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-  
+  async: true,
+  crossDomain: true, // Corrected typo in "crossDomain"
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  method: "GET",
+  headers: {},
+};
+
+$.ajax(settings).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
