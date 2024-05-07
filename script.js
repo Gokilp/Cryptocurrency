756344d7c417bb0968ce5de2e2d9175ef17b8475
gokilp <gokilp25@gmail.com>
2024-05-07 08:37:50 +0530

    fix api error

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var settings = {
     "async": true,
-    "crossDomain": true,
-    "url": 
-    "headers": {
-    }
-}
+    "crossDomain": true, // Corrected typo in "crossDomain"
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "method": "GET",
+    "headers": {}
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+  
